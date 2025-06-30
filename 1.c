db6f9758b6a3978576d5682f44285b1a2667ad66
Pavlo <ist24-p.kedych@nubip.edu.ua>
2025-07-01 01:14:05 +0300

    Практичне завдання №1

---
+++ 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int t1, t2, t3;
+    scanf("%d %d %d", &t1, &t2, &t3);
+
+    double total_time = 1.0 / (1.0 / t1 + 1.0 / t2 + 1.0 / t3);
+    printf("%.2lf\n", total_time);
+
+    return 0;
+}
